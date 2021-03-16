467f6d9ec21ef1636a6be59f77fcc279f0cff620
Shivansh Vij <shivanshvij@loopholelabs.io>
2021-03-16 01:12:57 -0400

    11ns encoding (with separate write method)

--- internal/protocol/protocol.go
+++ internal/protocol/protocol.go
@@ -3,6 +3,8 @@ package protocol
 import (
 	"encoding/binary"
 	"github.com/pkg/errors"
+	"io"
+	"unsafe"
 )
 
 const (
@@ -26,7 +28,6 @@ type MessageV0 struct {
 	Operation     uint16 // 2 Byte
 	Routing       uint32 // 4 Bytes
 	ContentLength uint32 // 4 Bytes
-	Content       []byte
 }
 
 type V0Handler struct{}
@@ -39,109 +40,105 @@ func NewV0Handler() V0Handler {
 	return V0Handler{}
 }
 
-func (handler *V0Handler) Encode(operation uint16, routing uint32, buf []byte) ([]byte, error) {
+func (handler *V0Handler) Encode(operation uint16, routing uint32, contentLength uint32) ([HeaderLengthV0]byte, error) {
 	message := MessageV0{
 		Version:       Version0,
 		Operation:     operation,
 		Routing:       routing,
-		ContentLength: uint32(len(buf)),
-		Content:       buf,
+		ContentLength: contentLength,
 	}
 
 	return message.Encode()
 }
 
 func (handler *V0Handler) Decode(buf []byte) (message MessageV0, err error) {
-	message = MessageV0{
-		Content: buf,
+	if len(buf) < HeaderLengthV0 {
+		return MessageV0{}, errors.New("Invalid Buffer Length")
 	}
-	err = message.Decode(false)
+
+	err = message.Decode(*(*[HeaderLengthV0]byte)(unsafe.Pointer(&buf[0])))
+
+	return
+}
+
+func (handler *V0Handler) Write(message [HeaderLengthV0]byte, content *[]byte, destination io.Writer) (n int, err error) {
+	n, err = destination.Write(message[:])
+	m, err := destination.Write(*content)
+	n += m
 
 	return
 }
 
 // Encode MessageV0
-func (fm *MessageV0) Encode() (result []byte, err error) {
+func (fm *MessageV0) Encode() (result [HeaderLengthV0]byte, err error) {
 	defer func() {
 		if recoveredErr := recover(); recoveredErr != nil {
 			err = errors.Wrap(recoveredErr.(error), "Error Encoding V0 Message")
 		}
 	}()
 
-	intermediate := make([]uint8, HeaderLengthV0)
-	intermediate[0] = uint8(0x00) // Reserved
-	intermediate[1] = uint8(0x46) // F
-	intermediate[2] = uint8(0x42) // B
-	intermediate[3] = uint8(0x45) // E
-	intermediate[4] = uint8(0x45) // E
-	intermediate[5] = fm.Version
-	binary.BigEndian.PutUint16(intermediate[6:8], fm.Operation)
-	binary.BigEndian.PutUint32(intermediate[8:12], fm.Routing)
-	binary.BigEndian.PutUint32(intermediate[12:16], fm.ContentLength)
-
-	result = make([]byte, HeaderLengthV0+fm.ContentLength)
-	copy(result[:HeaderLengthV0], intermediate)
-
-	if fm.ContentLength > 0 {
-		copy(result[HeaderLengthV0:], fm.Content)
-	}
+	result[0] = byte(0x00) // Reserved
+	result[1] = byte(0x46) // F
+	result[2] = byte(0x42) // B
+	result[3] = byte(0x45) // E
+	result[4] = byte(0x45) // E
+	result[5] = fm.Version
+	binary.BigEndian.PutUint16(result[6:8], fm.Operation)
+	binary.BigEndian.PutUint32(result[8:12], fm.Routing)
+	binary.BigEndian.PutUint32(result[12:16], fm.ContentLength)
 
 	return
 }
 
 // Decode MessageV0
-func (fm *MessageV0) Decode(headerOnly bool) (err error) {
+func (fm *MessageV0) Decode(buf [HeaderLengthV0]byte) (err error) {
 	defer func() {
 		if recoveredErr := recover(); recoveredErr != nil {
 			err = errors.Wrap(recoveredErr.(error), "Error Decoding V0 Message")
 		}
 	}()
 
-	if len(fm.Content) < HeaderLengthV0 {
-		return errors.New("Invalid Message Header (Too Small)")
-	}
-
-	fm.Version = fm.Content[5]
+	fm.Version = buf[5]
 	if !validVersion(fm.Version) {
 		return errors.New("Invalid Message Version")
 	}
 
-	fm.Operation = binary.BigEndian.Uint16(fm.Content[6:8])
-	fm.Routing = binary.BigEndian.Uint32(fm.Content[8:12])
-	fm.ContentLength = binary.BigEndian.Uint32(fm.Content[12:16])
-
-	if fm.ContentLength > 0 && !headerOnly {
-		fm.Content = fm.Content[HeaderLengthV0:]
-		if uint32(len(fm.Content)) != fm.ContentLength {
-			return errors.New("Invalid Data is not the same length as Data Length")
-		}
-	} else if fm.ContentLength < 1 {
-		fm.Content = make([]byte, 0)
-	}
+	fm.Operation = binary.BigEndian.Uint16(buf[6:8])
+	fm.Routing = binary.BigEndian.Uint32(buf[8:12])
+	fm.ContentLength = binary.BigEndian.Uint32(buf[12:16])
 
 	return nil
 }
 
 // EncodeV0 without a Handler
-func EncodeV0(operation uint16, routing uint32, buf []byte) ([]byte, error) {
+func EncodeV0(operation uint16, routing uint32, contentLength uint32) ([HeaderLengthV0]byte, error) {
 	message := MessageV0{
 		Version:       Version0,
 		Operation:     operation,
 		Routing:       routing,
-		ContentLength: uint32(len(buf)),
-		Content:       buf,
+		ContentLength: contentLength,
 	}
 
 	return message.Encode()
 }
 
+func Write(message [HeaderLengthV0]byte, content *[]byte, destination io.Writer) (n int, err error) {
+	n, err = destination.Write(message[:])
+	m, err := destination.Write(*content)
+	n += m
+
+	return
+}
+
 // DecodeV0 without a Handler
-func DecodeV0(buf []byte, headerOnly bool) (message MessageV0, err error) {
-	message = MessageV0{
-		Content: buf,
+func DecodeV0(buf []byte) (message MessageV0, err error) {
+	if len(buf) < HeaderLengthV0 {
+		return MessageV0{}, errors.New("Invalid Buffer Length")
 	}
 
-	return message, message.Decode(headerOnly)
+	err = message.Decode(*(*[HeaderLengthV0]byte)(unsafe.Pointer(&buf[0])))
+
+	return
 }
 
 func validVersion(version uint8) bool {
--- test/internal/protocol_test.go
+++ test/internal/protocol_test.go
@@ -1,8 +1,10 @@
 package test
 
 import (
+	"bytes"
 	"crypto/rand"
 	"github.com/loophole-labs/frisbee/internal/protocol"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -12,88 +14,155 @@ func TestDefaultHandler(t *testing.T) {
 	assert.Equal(t, defaultHandler, protocol.NewV0Handler())
 }
 
-func TestEncodeDecodeV0(t *testing.T) {
-	randomData := make([]byte, 512)
-	_, _ = rand.Read(randomData)
-
+func TestEncodeDecodeHandlerV0(t *testing.T) {
 	handlerV0 := protocol.NewV0Handler()
 
-	encodedBytes, err := handlerV0.Encode(protocol.MessagePacket, 0, randomData)
+	encodedBytes, err := handlerV0.Encode(protocol.MessagePacket, 0, 512)
 	assert.Equal(t, nil, err)
 
-	message, err := handlerV0.Decode(encodedBytes)
+	message, err := handlerV0.Decode(encodedBytes[:])
 	assert.Equal(t, nil, err)
 	assert.Equal(t, uint32(512), message.ContentLength)
 	assert.Equal(t, protocol.Version0, message.Version)
 	assert.Equal(t, uint32(0), message.Routing)
 	assert.Equal(t, protocol.MessagePacket, message.Operation)
-	assert.Equal(t, randomData, message.Content)
-}
 
-func TestUnsafeEncodeDecodeV0(t *testing.T) {
-
-	randomData := make([]byte, 512)
-	_, _ = rand.Read(randomData)
-
-	handlerV0 := protocol.NewV0Handler()
-
-	encodedBytes, err := handlerV0.Encode(protocol.MessagePacket, 0, randomData)
+	emptyEncodedBytes, err := handlerV0.Encode(protocol.MessagePing, 0, 0)
 	assert.Equal(t, nil, err)
 
-	message, err := handlerV0.Decode(encodedBytes)
-	assert.Equal(t, nil, err)
-	assert.Equal(t, uint32(512), message.ContentLength)
-	assert.Equal(t, protocol.Version0, message.Version)
-	assert.Equal(t, uint32(0), message.Routing)
-	assert.Equal(t, protocol.MessagePacket, message.Operation)
-	assert.Equal(t, randomData, message.Content)
-
-	emptyData := make([]byte, 0)
-	emptyEncodedBytes, err := handlerV0.Encode(protocol.MessagePing, 0, emptyData)
-	assert.Equal(t, nil, err)
-
-	emptyMessage, err := handlerV0.Decode(emptyEncodedBytes)
+	emptyMessage, err := handlerV0.Decode(emptyEncodedBytes[:])
 	assert.Equal(t, nil, err)
 	assert.Equal(t, uint32(0), emptyMessage.ContentLength)
 	assert.Equal(t, protocol.Version0, emptyMessage.Version)
 	assert.Equal(t, uint32(0), emptyMessage.Routing)
 	assert.Equal(t, protocol.MessagePing, emptyMessage.Operation)
-	assert.Equal(t, emptyData, emptyMessage.Content)
+
+	invalidMessage, err := handlerV0.Decode(emptyEncodedBytes[1:])
+	assert.NotEqual(t, nil, err)
+	assert.Equal(t, errors.New("Invalid Buffer Length").Error(), err.Error())
+	assert.Equal(t, uint32(0), invalidMessage.ContentLength)
+	assert.Equal(t, uint8(0), invalidMessage.Version)
+	assert.Equal(t, uint32(0), invalidMessage.Routing)
+	assert.Equal(t, uint16(0), invalidMessage.Operation)
+
 }
 
-func BenchmarkEncode(b *testing.B) {
+func TestEncodeDecodeV0(t *testing.T) {
+	encodedBytes, err := protocol.EncodeV0(protocol.MessagePacket, 0, 512)
+	assert.Equal(t, nil, err)
+
+	message, err := protocol.DecodeV0(encodedBytes[:])
+	assert.Equal(t, nil, err)
+	assert.Equal(t, uint32(512), message.ContentLength)
+	assert.Equal(t, protocol.Version0, message.Version)
+	assert.Equal(t, uint32(0), message.Routing)
+	assert.Equal(t, protocol.MessagePacket, message.Operation)
+
+	emptyEncodedBytes, err := protocol.EncodeV0(protocol.MessagePing, 0, 0)
+	assert.Equal(t, nil, err)
+
+	emptyMessage, err := protocol.DecodeV0(emptyEncodedBytes[:])
+	assert.Equal(t, nil, err)
+	assert.Equal(t, uint32(0), emptyMessage.ContentLength)
+	assert.Equal(t, protocol.Version0, emptyMessage.Version)
+	assert.Equal(t, uint32(0), emptyMessage.Routing)
+	assert.Equal(t, protocol.MessagePing, emptyMessage.Operation)
+
+	invalidMessage, err := protocol.DecodeV0(emptyEncodedBytes[1:])
+	assert.NotEqual(t, nil, err)
+	assert.Equal(t, errors.New("Invalid Buffer Length").Error(), err.Error())
+	assert.Equal(t, uint32(0), invalidMessage.ContentLength)
+	assert.Equal(t, uint8(0), invalidMessage.Version)
+	assert.Equal(t, uint32(0), invalidMessage.Routing)
+	assert.Equal(t, uint16(0), invalidMessage.Operation)
+}
+
+func TestWriteHandler(t *testing.T) {
+	handlerV0 := protocol.NewV0Handler()
+
 	randomData := make([]byte, 512)
 	_, _ = rand.Read(randomData)
+
+	encodedBytes, err := handlerV0.Encode(protocol.MessagePacket, 0, 512)
+	assert.Equal(t, nil, err)
+
+	correctData := append(encodedBytes[:], randomData...)
+
+	var buf bytes.Buffer
+
+	n, err := handlerV0.Write(encodedBytes, &randomData, &buf)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, protocol.HeaderLengthV0+512, n)
+
+	assert.Equal(t, correctData, buf.Bytes())
+}
+
+func TestWrite(t *testing.T) {
+	randomData := make([]byte, 512)
+	_, _ = rand.Read(randomData)
+
+	encodedBytes, err := protocol.EncodeV0(protocol.MessagePacket, 0, 512)
+	assert.Equal(t, nil, err)
+
+	correctData := append(encodedBytes[:], randomData...)
+
+	var buf bytes.Buffer
+
+	n, err := protocol.Write(encodedBytes, &randomData, &buf)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, protocol.HeaderLengthV0+512, n)
+
+	assert.Equal(t, correctData, buf.Bytes())
+}
+
+func BenchmarkEncodeHandler(b *testing.B) {
 
 	handlerV0 := protocol.NewV0Handler()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _ = handlerV0.Encode(protocol.MessagePacket, 0, randomData)
+		_, _ = handlerV0.Encode(protocol.MessagePacket, 0, 512)
+	}
+}
+
+func BenchmarkDecodeHandler(b *testing.B) {
+	handlerV0 := protocol.NewV0Handler()
+	encodedMessage, _ := handlerV0.Encode(protocol.MessagePacket, 0, 512)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = handlerV0.Decode(encodedMessage[:])
+	}
+}
+
+func BenchmarkEncodeDecodeHandler(b *testing.B) {
+	handlerV0 := protocol.NewV0Handler()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		encodedMessage, _ := handlerV0.Encode(protocol.MessagePacket, 0, 512)
+		_, _ = handlerV0.Decode(encodedMessage[:])
+	}
+}
+
+func BenchmarkEncode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = protocol.EncodeV0(protocol.MessagePacket, 0, 512)
 	}
 }
 
 func BenchmarkDecode(b *testing.B) {
-	randomData := make([]byte, 512)
-	_, _ = rand.Read(randomData)
-
-	handlerV0 := protocol.NewV0Handler()
-	encodedMessage, _ := handlerV0.Encode(protocol.MessagePacket, 0, randomData)
+	encodedMessage, _ := protocol.EncodeV0(protocol.MessagePacket, 0, 512)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _ = handlerV0.Decode(encodedMessage)
+		_, _ = protocol.DecodeV0(encodedMessage[:])
 	}
 }
 
 func BenchmarkEncodeDecode(b *testing.B) {
-	randomData := make([]byte, 512)
-	_, _ = rand.Read(randomData)
-
-	handlerV0 := protocol.NewV0Handler()
-
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		encodedMessage, _ := handlerV0.Encode(protocol.MessagePacket, 0, randomData)
-		_, _ = handlerV0.Decode(encodedMessage)
+		encodedMessage, _ := protocol.EncodeV0(protocol.MessagePacket, 0, 512)
+		_, _ = protocol.DecodeV0(encodedMessage[:])
 	}
 }
