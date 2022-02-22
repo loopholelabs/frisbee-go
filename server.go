c880d59c12b18bc9af5816f390d59db9351103c4
Shivansh Vij <shivanshvij@loopholelabs.io>
2022-02-21 19:13:51 -0500

    Adding connection closer hook for when connection is closed from the client side

--- server.go
+++ server.go
@@ -145,11 +145,9 @@ LOOP:
 func (s *Server) connCloser(conn *Async) {
 	select {
 	case <-conn.CloseChannel():
-		s.OnClosed(conn, conn.Error())
 		s.wg.Done()
 	case <-s.shutdownCh:
 		_ = conn.Close()
-		s.OnClosed(conn, nil)
 		s.wg.Done()
 	}
 }
