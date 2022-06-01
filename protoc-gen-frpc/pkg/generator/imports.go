72b10e7877acf3ab1dde56767ff31e7451aaa3be
Alex Sørlie Glomsaas <alex@sorlie.io>
2022-06-01 17:50:55 +0200

    Implement streams in FRPC

--- protoc-gen-frpc/pkg/generator/imports.go
+++ protoc-gen-frpc/pkg/generator/imports.go
@@ -24,6 +24,7 @@ var (
 
 	serviceImports = []string{
 		"github.com/loopholelabs/frisbee",
+		"github.com/loopholelabs/frisbee/internal/queue",
 		"github.com/rs/zerolog",
 		"crypto/tls",
 		"context",
