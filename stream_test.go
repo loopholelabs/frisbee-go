573b508f6b3fdceb816b3815703b1a538d338d6f
Shivansh Vij <shivanshvij@outlook.com>
2022-09-27 00:02:17 -0400

    Adding a test case to guarantee that both sides can open the stream and things will work exactly as expected

--- stream_test.go
+++ stream_test.go
@@ -153,3 +153,64 @@ func TestNewStreamStale(t *testing.T) {
 	err = readerConn.Close()
 	assert.NoError(t, err)
 }
+
+func TestNewStreamDualCreate(t *testing.T) {
+	t.Parallel()
+
+	const packetSize = 512
+
+	emptyLogger := zerolog.New(io.Discard)
+
+	reader, writer := net.Pipe()
+
+	readerConn := NewAsync(reader, &emptyLogger)
+	writerConn := NewAsync(writer, &emptyLogger)
+
+	writerStream := writerConn.Stream(0)
+	readerStream := readerConn.Stream(0)
+
+	data := make([]byte, packetSize)
+	_, err := rand.Read(data)
+	require.NoError(t, err)
+
+	p := packet.Get()
+	p.Metadata.Id = 64
+	p.Metadata.Operation = 32
+	p.Metadata.ContentLength = uint32(packetSize)
+	p.Content.Write(data)
+
+	err = writerStream.WritePacket(p)
+	require.NoError(t, err)
+
+	err = writerStream.WritePacket(p)
+	require.NoError(t, err)
+
+	packet.Put(p)
+
+	err = writerStream.Close()
+	require.NoError(t, err)
+
+	time.Sleep(DefaultDeadline)
+
+	p, err = readerStream.ReadPacket()
+	require.NoError(t, err)
+	require.NotNil(t, p.Metadata)
+	assert.Equal(t, readerStream.ID(), p.Metadata.Id)
+	assert.Equal(t, STREAM, p.Metadata.Operation)
+	assert.Equal(t, uint32(packetSize), p.Metadata.ContentLength)
+	assert.Equal(t, data, p.Content.Bytes())
+
+	p, err = readerStream.ReadPacket()
+	require.NoError(t, err)
+	require.NotNil(t, p.Metadata)
+	assert.Equal(t, readerStream.ID(), p.Metadata.Id)
+	assert.Equal(t, STREAM, p.Metadata.Operation)
+	assert.Equal(t, uint32(packetSize), p.Metadata.ContentLength)
+	assert.Equal(t, data, p.Content.Bytes())
+
+	_, err = readerStream.ReadPacket()
+	require.ErrorIs(t, err, StreamClosed)
+
+	err = readerConn.Close()
+	assert.NoError(t, err)
+}
